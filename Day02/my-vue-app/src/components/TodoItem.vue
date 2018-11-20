8449ecf224f5555bf4738cc7a8a610910aa6a1cb
Arnav Gupta <arnav@codingblocks.com>
2018-11-20 12:05:59 +0530

    components inside todolist
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Day02/my-vue-app/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li class="todoitem">
+    <slot></slot>
+    {{item.task}}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    item: Object
+  }
+}
+</script>
+
--- Day02/my-vue-app/src/views/Todo.vue
+++ Day02/my-vue-app/src/views/Todo.vue
@@ -4,25 +4,38 @@
     <input v-model="newtask">
     <button @click="addTask">ADD</button>
     <ul>
-      <li v-for="todo in todos">
-        {{todo}}
-      </li>
+      <TodoItem v-for="todo in todos" v-bind:item="todo">
+        {{todo.id}}
+      </TodoItem>
     </ul>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem.vue'
 export default {
   data: function () {
     return ({
       newtask: '',
-      todos: ['first task', 'second task']
+      todos: [
+        {id: 1, task: 'first task', done: false},
+        {id: 2, task: 'second task', done: true},
+      ]
     })
   },
   methods: {
     addTask() {
       this.todos.push(this.newtask)
     }
+  },
+  components: {
+    TodoItem
   }
 }
 </script>
+
+<style>
+.todoitem {
+  font-size: 20pt;
+}
+</style>
