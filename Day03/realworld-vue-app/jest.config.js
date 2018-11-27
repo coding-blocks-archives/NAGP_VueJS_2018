088cbafc2db03d11f838636cf5fb8ce4f702863d
Arnav Gupta <arnav@codingblocks.com>
2018-11-27 11:06:07 +0530

    default project generated by vue cli
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day03/realworld-vue-app/jest.config.js
+++ Day03/realworld-vue-app/jest.config.js
@@ -13,8 +13,5 @@ module.exports = {
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)"
   ],
-  testURL: "http://localhost/",
-  collectCoverage: true,
-  collectCoverageFrom: ["**/*.{js,vue}", "!**/node_modules/**"],
-  coverageReporters: ["html", "text-summary"]
+  testURL: "http://localhost/"
 };
