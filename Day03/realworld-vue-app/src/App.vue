3de82b569a871321308d3f8c9fe9416419a906c1
Arnav Gupta <arnav@codingblocks.com>
2018-11-27 11:15:27 +0530

    get the css working with homepage
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day03/realworld-vue-app/src/App.vue
+++ Day03/realworld-vue-app/src/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <NavBar></NavBar>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import NavBar from '@/components/NavBar.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  components: {
+    NavBar, Footer
+  }
 }
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
